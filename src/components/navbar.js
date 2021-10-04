9f809f614ac4f5d13961c90c58191319f7319ca3
Muhdan Fyan Syah Sofian <muhdanfyan@gmail.com>
2021-10-04 20:08:09 +0800

    navbar 1

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,22 +2,28 @@ import React, { Component } from "react"
 import { Link } from "react-router-dom"
 import Styles from "./style/navbar.module.css"
 import Logo from "../assets/logo.png"
+import { FaShoppingCart } from "react-icons/fa"
 
 export default class Navbar extends Component{
     render(){
         return(
             <div className={Styles.header}>
-                <div>
-                    <img src={Logo} alt="Logo" />
+                <div className={Styles.container}>
+                    <div>
+                        <img className={Styles.logo} src={Logo} alt="Logo" />
+                    </div>
+                    <ul className={Styles.topmenu}>
+                        <li> <Link to="/">Homepage</Link> </li>
+                        <li> <Link to="/produk">Page</Link> </li>
+                        <li> <Link to="/produk/:url">Transaksi</Link> </li>
+                        <li> <Link to="/kontak">Kontak</Link> </li>
+                        <li> <Link to="/tentang">Tentang</Link> </li>
+                        <li> <Link to="/keranjang">Keranjang</Link> </li>
+                    </ul>
+                    <div className={Styles.cart}>
+                        <FaShoppingCart />
+                    </div>
                 </div>
-                <ul>
-                    <li> <Link to="/">Homepage</Link> </li>
-                    <li> <Link to="/produk">Page</Link> </li>
-                    <li> <Link to="/produk/:url">Transaksi</Link> </li>
-                    <li> <Link to="/kontak">Kontak</Link> </li>
-                    <li> <Link to="/tentang">Tentang</Link> </li>
-                    <li> <Link to="/keranjang">Keranjang</Link> </li>
-                </ul>
             </div>
         )
     }
