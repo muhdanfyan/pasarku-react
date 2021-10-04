3e89bdc0084b38729cf0ecb9be8ab38f4d3ec765
Muhdan Fyan Syah Sofian <muhdanfyan@gmail.com>
2021-10-04 20:35:36 +0800

    navbar updt 2

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
+import Style from '../components/style/global.module.css'
 
 class Homepage extends Component {
     render() {
         return (
-            <div>
-                Ini halaman homepage
-            </div>
+            <React.Fragment>
+                <div className={`${Style.wrapper} ${Style.banner_1}`}></div>
+                <div className={Style.wrapper}>
+                    <div className={StyleSheet.row}>
+                        <div className={Style.card}>
+                            <input type="search" placeholder="Cari Produk" />
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
         )
     }
 }
