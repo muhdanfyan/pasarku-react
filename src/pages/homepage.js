4d14bb8fda9d870d0ea33a00febb939ed0e0490e
Muhdan Fyan Syah Sofian <muhdanfyan@gmail.com>
2021-10-08 16:52:20 +0800

    update

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react'
 import Style from '../components/style/global.module.css'
+import produk_1 from "../assets/produk/1.png"
+import produk_2 from "../assets/produk/2.png"
+import produk_3 from "../assets/produk/3.png"
+import produk_4 from "../assets/produk/4.png"
+import produk_5 from "../assets/produk/5.png"
+import produk_6 from "../assets/produk/6.png"
+import produk_7 from "../assets/produk/7.png"
+import produk_8 from "../assets/produk/8.png"
 
 class Homepage extends Component {
     render() {
@@ -18,28 +26,52 @@ class Homepage extends Component {
                     <div className={Style.row}>
                         <div className={Style.grid_produk}>
                             <div className={Style.item_produk}>
-                                NAMA PRODUK
+                                <img src={produk_1} alt="Item Produk" />
+                                <h2>Nike Air Max 270 React</h2>
+                                <h4>IDR. 1.200.000,-</h4>
+                                <button>BELI</button>
                             </div>
                             <div className={Style.item_produk}>
-                                NAMA PRODUK
+                                <img src={produk_2} alt="Item Produk" />
+                                <h2>Nike Air Max 270 React</h2>
+                                <h4>IDR. 1.200.000,-</h4>
+                                <button>BELI</button>
                             </div>
                             <div className={Style.item_produk}>
-                                NAMA PRODUK
+                                <img src={produk_3} alt="Item Produk" />
+                                <h2>Nike Air Max 270 React</h2>
+                                <h4>IDR. 1.200.000,-</h4>
+                                <button>BELI</button>
                             </div>
                             <div className={Style.item_produk}>
-                                NAMA PRODUK
+                                <img src={produk_4} alt="Item Produk" />
+                                <h2>Nike Air Max 270 React</h2>
+                                <h4>IDR. 1.200.000,-</h4>
+                                <button>BELI</button>
                             </div>
                             <div className={Style.item_produk}>
-                                NAMA PRODUK
+                                <img src={produk_5} alt="Item Produk" />
+                                <h2>Nike Air Max 270 React</h2>
+                                <h4>IDR. 1.200.000,-</h4>
+                                <button>BELI</button>
                             </div>
                             <div className={Style.item_produk}>
-                                NAMA PRODUK
+                                <img src={produk_6} alt="Item Produk" />
+                                <h2>Nike Air Max 270 React</h2>
+                                <h4>IDR. 1.200.000,-</h4>
+                                <button>BELI</button>
                             </div>
                             <div className={Style.item_produk}>
-                                NAMA PRODUK
+                                <img src={produk_7} alt="Item Produk" />
+                                <h2>Nike Air Max 270 React</h2>
+                                <h4>IDR. 1.200.000,-</h4>
+                                <button>BELI</button>
                             </div>
                             <div className={Style.item_produk}>
-                                NAMA PRODUK
+                                <img src={produk_8} alt="Item Produk" />
+                                <h2>Nike Air Max 270 React</h2>
+                                <h4>IDR. 1.200.000,-</h4>
+                                <button>BELI</button>
                             </div>
                         </div>
                     </div>
